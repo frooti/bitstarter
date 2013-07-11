d7022d10e203e936195ca9cdf6cdd87243358227
frooti <ravi.muppalaneni@gmail.com>
2013-07-11 07:07:52 +0000

    read from file

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
+var fs = require('fs');
+var buf = fs.readFileSync("index.html");
+var msg = buf.toString()
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(msg);
 });
 
 var port = process.env.PORT || 5000;

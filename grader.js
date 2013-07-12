1392a196c3fcbfe0e5574f57423a7434c422eed2
frooti <ravi.muppalaneni@gmail.com>
2013-07-12 22:00:17 +0000

    added auto_grader

---
+++ grader.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var program = require('commander');
+var cheerio = require('cheerio');
+var rest = require('restler');
+var URL_DEFAULT = "http://fierce-reaches-1073.herokuapp.com";
+var CHECKSFILE_DEFAULT = "checks.json";
+
+var assertFileExists = function(infile) {
+    var instr = infile.toString();
+    if(!fs.existsSync(instr)) {
+        console.log("%s does not exist. Exiting.", instr);
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    }
+    return instr;
+};
+
+var assertUrlExists = function(url) {
+    return true;
+};
+
+var loadChecks = function(checksfile) {
+    return JSON.parse(fs.readFileSync(checksfile));
+};
+
+var checkHtmlFile = function(url, checksfile) {
+    rest.get(url).on('complete', function(result,response) {
+	if (result instanceof Error) {
+            console.log('Error: ' + util.format(response.message));
+	    process.exit(1);
+         }
+	else {
+	    $ = cheerio.load(result);
+	    var checks = loadChecks(checksfile).sort();
+	    var out = {};
+	    for(var ii in checks) {
+		var present = $(checks[ii]).length > 0;
+		out[checks[ii]] = present;
+		}
+	    var outJson = JSON.stringify(out, null, 4);
+	    console.log(outJson);
+	    }
+	});
+};
+
+var clone = function(fn) {
+    // Workaround for commander.js issue.
+    // http://stackoverflow.com/a/6772648
+    return fn.bind({});
+};
+
+if(require.main == module) {
+    program
+        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-u, --url <url>', 'url of file', clone(assertUrlExists), URL_DEFAULT)
+        .parse(process.argv);
+    checkHtmlFile(program.url, program.checks);
+} else {
+    exports.checkHtmlFile = checkHtmlFile;
+}
